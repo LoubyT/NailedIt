0c5bddf46250509a53bdb689ecea60d32f9865cf
Louby <louby.trahan@gmail.com>
2022-06-26 17:32:48 -0500

    .

--- common/commerce.js
+++
@@ -1,5 +0,0 @@
-// Import the Commerce module
-import Commerce from '@chec/commerce.js';
-
-// Create a Commerce instance
-const commerce = new Commerce('{pk_444258dba16f62dbc96c94b47fe02b1b68687cf7e698e}');
